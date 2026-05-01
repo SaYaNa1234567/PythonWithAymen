475e88181edc79edad3d7279f29a5e88b4009f76
Galover <121336430+SaYaNa1234567@users.noreply.github.com>
2026-05-01 20:32:49 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 def pig_it(text):
-  words = text.split()
+  words = str(text.split())
   res = []
   for x in words:
     if x.isalpha():
-      pig = words[1:] +words[0]+"ay"
-      result.append(pig)
+      pigi = words[1:] +words[0]+'ay'
+      res.append(pigi)
     else:
-      result.append(pig)
+      res.append(x)
   return res
 
 pig_it('hi sanya')
-pig_it("i am not sanya:0")
+pig_it('i am not sanya:0')
