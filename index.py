9a84e9535df4e4bb4b126ca74e6e0ae5f2ffec8f
aymenezz <aymenezz555@gmail.com>
2026-05-01 14:38:37 +0300

    first task

--- index.py
+++ index.py
@@ -1,1 +1,19 @@
-print('hi sanya')
+'''
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+pig_it('Pig latin is cool') # igPay atinlay siay oolcay
+pig_it('Hello world !')     # elloHay orldway !
+'''
+
+name=input('words:').split()
+empty='ay'
+newword=''
+for i in range(len(name)):
+    newword=name[i]+name[i][0]+empty
+    newword=newword[1:]
+    print(newword)
+    
+
+    
+
+    
+
